3e4fbd83beabd1d33c42c4c477d3146379cb0f3a
Sean Wong <sean.uuong@gmail.com>
2022-11-20 11:39:32 -0800

    Complete publishing dance party

--- src/slidingDancer.js
+++ src/slidingDancer.js
@@ -2,6 +2,8 @@ var makeSlidingDancer = function(top, left, r, g, b, timeBetweenSteps, direction
   makeDancer.call(this, top, left, r, g, b, timeBetweenSteps, direction);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  this.$node.prepend('<img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/DCTM_Penguin_UK_DK_AL645784_zcau0r.jpg" width="10%" height="10%"/>')
+  this.$node.css({border: '0px'});
   this.direction = direction;
 
   // var oldStep = this.step;
